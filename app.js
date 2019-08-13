1cb10c418cefdcc1939128439550da29ec5e51ce
Vasilev Anatoliy <toliankir@gmail.com>
2019-08-13 14:46:30 +0300

    remove morgan from project

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require('path');
 const ejs = require('ejs');
 const fs = require('fs');
 const bodyParser = require('body-parser');
-const morgan = require('morgan');
+// const morgan = require('morgan');
 
 const db = require('./db');
 const logger = require('./winston');
@@ -13,34 +13,31 @@ db.initDB();
 
 const server = express();
 server.set('view engine', 'ejs');
-server.use(morgan('dev', { stream: logger.stream }));
+// server.use(morgan('dev', { stream: logger.stream }));
+server.use(logger);
 server.use(express.static(path.join(__dirname, 'public')));
 server.use(bodyParser.urlencoded({ extended: false }));
 server.use(bodyParser.json());
 
 
 server.get('/', (req, res) => {
-    logger.log({ level: 'info', message: 'Index request' });
     res.render('index', { users: db.getDB() })
 });
 
 server.post('/api', (req, res) => {
-    logger.log({ level: 'info', message: 'API POST request' });
     db.addUser(req.body.login, req.body.password);
     res.send(JSON.stringify({ status: `User ${req.body.login} added.` }));
 });
 
 server.get('/api', (req, res) => {
-    logger.log({ level: 'info', message: 'API GET request' });
     res.send(JSON.stringify(db.getDB()));
 });
 
 server.delete('/api', (req, res) => {
-    logger.log({ level: 'info', message: 'API DELETE request' });
     db.deleteUser(req.body.id);
     res.send(JSON.stringify({ status: `Delete ${req.body.id}` }));
 });
 
 server.listen(process.env.PORT, () => {
-    logger.log({ level: 'info', message: `Server start on ${process.env.PORT} port.` });
+    logger.winstonLogger.log({ level: 'info', message: `Server start on ${process.env.PORT} port.` });
 });
--- winston.js
+++ winston.js
@@ -1,9 +1,17 @@
 const winston = require('winston');
 const path = require('path');
+
 require('winston-daily-rotate-file');
 
-
-const logger = new winston.createLogger({
+const winstonLogger = new winston.createLogger({
+    format: winston.format.combine(
+        winston.format.timestamp({
+            format: "YY-MM-DD HH:MM:SS"
+        }),
+        winston.format.printf(info => {
+            return `${info.level}: ${info.timestamp} ${info.message}`;
+        })
+    ),
     transports: [
         new winston.transports.DailyRotateFile({
             filename: path.join(__dirname, 'logs', 'log-%DATE%.log'),
@@ -11,19 +19,23 @@ const logger = new winston.createLogger({
             format: winston.format.json()
         }),
         new winston.transports.Console({
-            level: 'debug',
             format: winston.format.combine(
                 winston.format.colorize({ all: true }),
-                winston.format.simple()
-            )
+            ),
+            level: 'debug',
         })
     ]
 });
 
-logger.stream = {
-    write: function (message, encoding) {
-        logger.info(message);
-    }
-};
+// logger.stream = {
+//     write: function (message, encoding) {
+//         logger.info(message);
+//     }
+// };
 
+const logger = (req, res, next) => {
+    winstonLogger.log('info', `${req.method} ${req.originalUrl}`);
+    next();
+};
 module.exports = logger;
+module.exports.winstonLogger = winstonLogger;
