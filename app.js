1ca13aaaa2b35cc6a485218314f59dea696b86f3
Vasilev Anatoliy <toliankir@gmail.com>
2019-08-13 10:14:37 +0300

    Add external db handler

--- app.js
+++ app.js
@@ -6,61 +6,42 @@ const fs = require('fs');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 
+const db = require('./db');
 const logger = require('./config/winston');
+
+db.initDB();
 
 const server = express();
 server.set('view engine', 'ejs');
-server.use(morgan('combined', {stream: logger.stream}));
+server.use(morgan('dev', { stream: logger.stream }));
 server.use(express.static(path.join(__dirname, 'public')));
-server.use(bodyParser.urlencoded({extended: false}));
+server.use(bodyParser.urlencoded({ extended: false }));
 server.use(bodyParser.json());
-
-const users = [];
-const dbFile = path.join(__dirname, process.env.DB_FILE);
-
-fs.access(dbFile, fs.constants.F_OK, (err) => {
-    if (err) {
-        return;
-    }
-    fs.readFile(dbFile, (err, data) => {
-        if (err) throw err;
-        users.push(...JSON.parse(data));
-    })
-});
 
 
 server.get('/', (req, res) => {
-    res.render('index', {users: users})
+    logger.log({ level: 'info', message: 'Index request' });
+    res.render('index', { users: db.getDB() })
 });
 
 server.post('/api', (req, res) => {
-    users.push({
-        id: users.length > 0 ? users[users.length - 1].id + 1 : 0,
-        login: req.body.login,
-        password: req.body.password
-    });
-    saveDB();
-    res.send(JSON.stringify({status: `User ${req.body.login} added.`}));
+    logger.log({ level: 'info', message: 'API POST request' });
+    db.addUser(req.body.login, req.body.password);
+    res.send(JSON.stringify({ status: `User ${req.body.login} added.` }));
 });
 
 server.get('/api', (req, res) => {
-    res.send(JSON.stringify(users));
+    logger.log({ level: 'info', message: 'API GET request' });
+    res.send(JSON.stringify(db.getDB()));
 });
 
 server.delete('/api', (req, res) => {
-    const userId = users.findIndex(user => {
-        return user.id == req.body.id;
-    });
-    users.splice(userId, 1);
-    saveDB();
-    res.send(JSON.stringify({
-        status: `Delete ${req.body.id}`
-    }));
+    logger.log({ level: 'info', message: 'API DELETE request' });
+    db.deleteUser(req.body.id);
+    res.send(JSON.stringify({ status: `Delete ${req.body.id}` }));
 });
 
-server.listen(process.env.PORT, () => console.log(`Server start on ${process.env.PORT} port.`));
+server.listen(process.env.PORT, () => {
+    logger.log({ level: 'info', message: `Server start on ${process.env.PORT} port.` });
+});
 
-function saveDB() {
-    fs.writeFile(dbFile, JSON.stringify(users), 'utf8', (err) => {
-    });
-}
--- config/winston.js
+++ config/winston.js
@@ -1,11 +1,14 @@
 const winston = require('winston');
 const path = require('path');
 
+
+
 const logger = new winston.createLogger({
+    
     transports: [
         new winston.transports.File(
             {
-                filename: path.join(__dirname, 'logs', 'all-logs.log'),
+                filename: path.join(__dirname, '../','logs', 'all-logs.log'),
                 level: 'info',
                 format: winston.format.json()
             }
@@ -13,7 +16,7 @@ const logger = new winston.createLogger({
         new winston.transports.Console({
             level: 'debug',
             format: winston.format.combine(
-                winston.format.colorize(),
+                winston.format.colorize({all: true}),
                 winston.format.simple()
             )
         })
