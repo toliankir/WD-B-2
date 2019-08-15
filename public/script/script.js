432048dd0fc6c823c7f667a5552b32630149c818
Vasilev Anatoliy <toliankir@gmail.com>
2019-08-16 00:16:19 +0300

    Change fetch to ajax in fronted js script

--- public/script/script.js
+++ public/script/script.js
@@ -2,42 +2,37 @@ const apiUrl = "./api";
 const userList = $("#user-list");
 
 async function syncUserList() {
-    const apiResponse = await fetch(apiUrl);
-    const users = await apiResponse.json();
+    const apiResponse = await $.ajax(apiUrl);
+    const users = JSON.parse(apiResponse);
     userList.html("");
-    users.forEach((user) => {
+    users.forEach(user => {
         userList.append($(`<li class="row p-1"><span class="col-3">${user.login}</span></li>`)
-            .append($('<a class="btn btn-danger btn-sm text-white">Delete</a>').on('click', () => {
-                deleteUserById(user.id);
+            .append($('<a class="btn btn-danger btn-sm text-white">Delete</a>').on('click', async () => {
+                await deleteUserById(user.id);
                 syncUserList();
             })));
     });
 }
 
-
 async function deleteUserById(id) {
-    const apiResponse = await fetch(apiUrl, {
+    await $.ajax({
         method: 'DELETE',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ id })
+        url: apiUrl,
+        data: { id }
     });
 }
 
 $("#user-add").on("submit", async (ev) => {
     ev.preventDefault();
 
-    const apiResponse = await fetch(apiUrl, {
+    await $.ajax({
         method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
+        url: apiUrl,
+        data: {
             login: $("#username").val(),
             password: $("#password").val()
-        })
-    });
+        }
+    })
     syncUserList();
 });
 
